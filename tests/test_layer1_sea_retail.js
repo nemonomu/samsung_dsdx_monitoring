ccbffb17a5c2121a99215d41fa67397de7e726de
Your Name <you@example.com>
2026-09-22 18:20:31 +0900

    test: update Layer 1 asset version expectation

--- tests/test_layer1_sea_retail.js
+++ tests/test_layer1_sea_retail.js
@@ -264,7 +264,7 @@ function categoryFromSummary(key) {
     assert(context.getStatusBadge('UNASSESSED').includes('미판정'));
     const baseSource = fs.readFileSync('apps/dx/dx_layer1/templates/base_layer1.html', 'utf8');
     assert(baseSource.includes("layer1.css' %}?v=4"));
-    assert(baseSource.includes("layer1-common.js' %}?v=20260922-volume1"));
+    assert(baseSource.includes("layer1-common.js' %}?v=20260922-next-day"));
     assert(baseSource.includes("collection-volume.js' %}?v=2"));
     assert(baseSource.includes("retail-status.js' %}?v=20260922-volume2"));
     assert(context.getStatusBadge('VOLUME_LOW').includes('이상'));
